f00564e00be606fcb09c5e40bca0e3a8b1305a8d
Jarcis-cy <jarcis-cy@qq.com>
2023-06-18 23:51:29 +0800

    [add] 添加词汇预设，便于筛选

--- tag_tools/process_files.py
+++ tag_tools/process_files.py
@@ -1,23 +1,34 @@
 import os
 from collections import Counter
 
+
 def process(folder_path, frequency, search_word, greater_than, less_than, delete_words):
     freq_counter = Counter()
-    search_counter = Counter() if search_word else None
-
     delete_word_list = delete_words.split(',') if delete_words else []
+
+    search_words = search_word.split(',') if search_word else []
+    search_counters = {word.strip(): Counter() for word in search_words}  # 为每个搜索词创建一个 Counter
 
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith('.txt'):
                 with open(os.path.join(root, file), 'r') as f:
                     content = f.read()
-                    words = [word.strip() for word in content.split(',') if word not in delete_word_list]  # 添加 strip 方法来删除前导和尾随空白，同时删除不想要的单词
+                    words = [word.strip() for word in content.split(',') if
+                             word not in delete_word_list]  # 添加 strip 方法来删除前导和尾随空白，同时删除不想要的单词
                     freq_counter.update(words)
-                    if search_word:
+                    for search_word, search_counter in search_counters.items():
                         search_counter.update(w for w in words if search_word in w)
 
-    freq_result = '\n'.join([f'"{word}": {count}' for word, count in freq_counter.items() if (greater_than and count > frequency) or (less_than and count < frequency) or count == frequency])
-    search_result = '\n'.join([f'"{word}": {count}' for word, count in search_counter.items()]) if search_word else ''
+    freq_result = '\n'.join([f'"{word}": {count}' for word, count in freq_counter.items() if
+                             (greater_than and count > frequency) or (
+                                         less_than and count < frequency) or count == frequency])
+
+    # 美化搜索结果的输出
+    # 美化搜索结果的输出
+    search_result = '\n\n'.join(
+        [f'{word}:\n' + '\n'.join([f'  {found_word}: {count}' for found_word, count in counter.items()]) for
+         word, counter in search_counters.items() if sum(counter.values()) > 0]) if search_words else ''
+
 
     return freq_result, search_result
--- tag_tools/ui.py
+++ tag_tools/ui.py
@@ -1,6 +1,10 @@
 import gradio as gr
 from tag_tools import process_files, replace_words, delete_words
 
+import gradio as gr
+from tag_tools import process_files, replace_words, delete_words
+
+
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as pro_interface:
         with gr.Row().style(equal_height=False):
@@ -9,37 +13,83 @@ def on_ui_tabs():
                     with gr.TabItem(label='Word Statistics'):
                         folder_input = gr.Textbox(label='Folder Path', lines=1)
                         frequency_input = gr.Slider(minimum=1, maximum=1000, step=1, label='Frequency', value=10)
-                        search_input = gr.Textbox(label='Search Word', lines=1)
+                        with gr.Row():
+                            search_input = gr.Textbox(label='Search Word', lines=1)
+
+                            options = ['None', 'Body', 'Face', 'Color', 'Sex']
+                            option_dropdown = gr.Dropdown(choices=options, label="Auto-fill Options")
+                            option_dropdown.change(auto_fill_search_words, inputs=[option_dropdown, search_input],
+                                                   outputs=search_input)
+
                         greater_than_checkbox = gr.Checkbox(label="Greater Than Frequency")
                         less_than_checkbox = gr.Checkbox(label="Less Than Frequency")
                         word_stats_button = gr.Button(value="Calculate Word Statistics")
-                        search_output = gr.Textbox(label="Search Results", interactive=False, visible=True, placeholder="Search results will be shown here")
+                        search_output = gr.Textbox(label="Search Results", interactive=False, visible=True,
+                                                   placeholder="Search results will be shown here")
                         with gr.Row():
-                            freq_output = gr.Textbox(label="Word Frequency", interactive=False, visible=True, placeholder="Word frequencies will be shown here")
-                            delete_words_input = gr.Textbox(label='Delete Words', lines=1, placeholder="Input words to delete, separated by comma")  # define delete_words_input here
+                            freq_output = gr.Textbox(label="Word Frequency", interactive=False, visible=True,
+                                                     placeholder="Word frequencies will be shown here")
+                            delete_words_input = gr.Textbox(label='Delete Words', lines=1,
+                                                            placeholder="Input words to delete, separated by comma")  # define delete_words_input here
                             freq_to_delete_button = gr.Button(value="Process Frequency to Delete Words")
-                            freq_to_delete_button.click(process_frequency_to_words, inputs=freq_output, outputs=delete_words_input)
+                            freq_to_delete_button.click(process_frequency_to_words, inputs=freq_output,
+                                                        outputs=delete_words_input)
                         delete_words_button = gr.Button(value="Delete Words")
                         delete_words_button.click(delete_words.delete_ws, inputs=[folder_input, delete_words_input])
-                        word_stats_button.click(process_files.process, inputs=[folder_input, frequency_input, search_input, greater_than_checkbox, less_than_checkbox], outputs=[freq_output, search_output])
-
-
-
+                        word_stats_button.click(process_files.process,
+                                                inputs=[folder_input, frequency_input, search_input,
+                                                        greater_than_checkbox, less_than_checkbox],
+                                                outputs=[freq_output, search_output])
                     with gr.TabItem(label='Replace Words'):
                         folder_input = gr.Textbox(label='Folder Path', lines=1)
-                        old_word_input = gr.Textbox(label='Old Word', lines=1, placeholder="If left empty, the New Word will be added to the end of all files.")
+                        old_word_input = gr.Textbox(label='Old Word', lines=1,
+                                                    placeholder="If left empty, the New Word will be added to the end of all files.")
                         new_word_input = gr.Textbox(label='New Word', lines=1)
                         global_replace_checkbox = gr.Checkbox(label="Global Replace")
                         replace_button = gr.Button(value="Replace Words")
-                        replace_output = gr.Textbox(label="Replace Status", interactive=False, visible=True, placeholder="Words replaced successfully")
-                        replace_button.click(replace_words.replace, inputs=[folder_input, old_word_input, new_word_input, global_replace_checkbox], outputs=replace_output)
+                        replace_output = gr.Textbox(label="Replace Status", interactive=False, visible=True,
+                                                    placeholder="Words replaced successfully")
+                        replace_button.click(replace_words.replace,
+                                             inputs=[folder_input, old_word_input, new_word_input,
+                                                     global_replace_checkbox], outputs=replace_output)
 
     return [(pro_interface, "Tag tools", "Tag tools")]
+
 
 def process_frequency_to_words(freq_str):
     lines = freq_str.strip().split('\n')
     words = [line.split(':')[0].strip().replace('"', '') for line in lines]
     return ','.join(words)
+
+
+def auto_fill_search_words(option, search_content):
+    body_words = 'arm,leg,head,body,hand,foot,finger,toe,chest,back,shoulder'
+    face_words = 'eye,nose,mouth,ear,cheek,chin,forehead,eyebrow,lip,teeth'
+    color_words = 'red,blue,green,yellow,black,white,pink,purple,orange,brown,grey'
+    sex_words = 'male,female,man,woman,boy,girl,he,she'
+
+    # Determine which words to add based on the selected option
+    if option == 'Body':
+        words_to_add = body_words
+    elif option == 'Face':
+        words_to_add = face_words
+    elif option == 'Color':
+        words_to_add = color_words
+    elif option == 'Sex':
+        words_to_add = sex_words
+    else:
+        words_to_add = ''
+
+    # If there's already content in the search box, add a comma before adding the new words
+    if search_content:
+        if not search_content.endswith(','):
+            search_content += ','
+        search_content += words_to_add
+    else:
+        search_content = words_to_add
+
+    return search_content
+
 
 if __name__ == "__main__":
     on_ui_tabs()
