d8049f0c8e3e1cce9d1d6152aa07b1c51335ea18
jarcis <779054469@qq.com>
2023-07-02 22:00:39 +0800

    [fix] 修改愚蠢的删除逻辑

--- tag_tools/delete_words.py
+++ tag_tools/delete_words.py
@@ -2,17 +2,23 @@ import os
 
 def delete_ws(folder_path, delete_words):
     delete_word_list = delete_words.split(',')
+    delete_word_list = [word.strip() for word in delete_word_list]
+
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith('.txt'):
                 with open(os.path.join(root, file), 'r') as f:
                     content = f.read()
-                # 删除内容中的单词
-                for word in delete_word_list:
-                    # 当单词后面有逗号时，同时删除单词和逗号
-                    content = content.replace(word.strip() + ',', '')
-                    # 当单词后面没有逗号时，只删除单词
-                    content = content.replace(word.strip(), '')
+
+                # 将文件内容转为单词数组
+                word_list = content.split(',')
+
+                # 生成一个新的单词数组，其中不包含待删除的单词
+                new_word_list = [word for word in word_list if word.strip() not in delete_word_list]
+
+                # 将新的单词数组连接为字符串，单词间以逗号分隔
+                new_content = ','.join(new_word_list)
+
                 # 将修改后的内容写回文件
                 with open(os.path.join(root, file), 'w') as f:
-                    f.write(content)
+                    f.write(new_content)
