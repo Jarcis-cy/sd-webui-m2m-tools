18e2c299a6d244516cc051f01f182cd78039a491
jarcis <779054469@qq.com>
2023-07-02 22:29:21 +0800

    [fix] 修改愚蠢的删除逻辑

--- tag_tools/delete_words.py
+++ tag_tools/delete_words.py
@@ -2,7 +2,7 @@ import os
 
 def delete_ws(folder_path, delete_words):
     delete_word_list = delete_words.split(',')
-    delete_word_list = [word.strip() for word in delete_word_list]
+    delete_word_list = sorted([word.strip() for word in delete_word_list], key=len, reverse=True)
 
     for root, dirs, files in os.walk(folder_path):
         for file in files:
@@ -10,15 +10,12 @@ def delete_ws(folder_path, delete_words):
                 with open(os.path.join(root, file), 'r') as f:
                     content = f.read()
 
-                # 将文件内容转为单词数组
-                word_list = content.split(',')
-
-                # 生成一个新的单词数组，其中不包含待删除的单词
-                new_word_list = [word for word in word_list if word.strip() not in delete_word_list]
-
-                # 将新的单词数组连接为字符串，单词间以逗号分隔
-                new_content = ','.join(new_word_list)
+                # 先处理长度较长的项
+                for word_to_delete in delete_word_list:
+                    word_list = content.split(',')
+                    new_word_list = [word for word in word_list if word.strip() != word_to_delete]
+                    content = ','.join(new_word_list)
 
                 # 将修改后的内容写回文件
                 with open(os.path.join(root, file), 'w') as f:
-                    f.write(new_content)
+                    f.write(content)
